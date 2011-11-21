d455ad7ff656286ea716fd60d16dc6abbf5fa8a6
Grzegorz Slowikowski <gslowikowski@gmail.com>
2011-11-21 08:53:55 +0000

    Play! Excel Module test project.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/trunk/test-projects@4956 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ external-modules/excel/1.2.3/excel/app/controllers/Application.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import java.util.Date;
+import java.util.List;
+
+import models.Contact;
+import play.Logger;
+import play.modules.excel.RenderExcel;
+import play.mvc.Controller;
+import play.mvc.With;
+
+@With(ExcelControllerHelper.class)
+public class Application extends Controller {
+
+    public static void index() {
+    	List<Contact> contacts = Contact.findAll();
+        render(contacts);
+    }
+    
+    public static void generateNameCard(Long id) {
+        Logger.info("generateNameCard");
+    	Contact person = Contact.findById(id);
+    	request.format = "xls";
+        renderArgs.put("__EXCEL_FILE_NAME__", person.getId() + ".xls");
+    	render(person);
+    }
+    
+    public static void generateAddressBook() {
+        Logger.info("generateAddressBook");
+    	List<Contact> contacts = Contact.findAll();
+    	Date date = new Date();
+    	String __EXCEL_FILE_NAME__ = "address_book.xlsx";
+    	renderArgs.put(RenderExcel.RA_ASYNC, true);
+        render(__EXCEL_FILE_NAME__, contacts, date);
+        Logger.info("generateAddressBook exit");
+    }
+
+}              
