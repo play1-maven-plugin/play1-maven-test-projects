440ed101d4ed1b224b7fc2c34b85a64c221436c6
Grzegorz Slowikowski <gslowikowski@gmail.com>
2011-12-05 09:49:56 +0000

    Ebean test projects.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/trunk/test-projects@5464 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ external-modules/ebean/ebean-yabe/test/ApplicationTest.java
@@ -0,0 +1,25 @@
+import org.junit.*;
+import play.test.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import models.*;
+
+public class ApplicationTest extends FunctionalTest {
+
+    @Test
+    public void testThatIndexPageWorks() {
+        Response response = GET("/");
+        assertIsOk(response);
+        assertContentType("text/html", response);
+        assertCharset("utf-8", response);
+    }
+    
+    @Test
+    public void testAdminSecurity() {
+        Response response = GET("/admin");
+        assertStatus(302, response);
+//GS        assertHeaderEquals("Location", "http://localhost/login", response);
+        assertHeaderEquals("Location", "/login", response);
+    }
+    
+}
