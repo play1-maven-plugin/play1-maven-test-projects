d53eb98ab47d78a08702137f6cf9490688d788d6
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-05-15 10:21:19 +0000

    play1-maven-plugin/play1-maven-plugin#71 - Mavenize Play! Japid Module v.0.9.3.4.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/trunk/test-projects@9196 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ external-modules/japid/japid-sample/app/japidviews/_javatags/dummy.java
@@ -0,0 +1,10 @@
+/*
+ * Package with at least one class required for javac to compile classes which import this package
+ * (which contain "import japidviews._javatags.*"). These are Japid generated classes.
+ * Javac cannot be configured to ignore this case (JDT compiler embedded in Play! is configured to ignore it).
+ */
+package japidviews._javatags;
+
+public class dummy
+{
+}
